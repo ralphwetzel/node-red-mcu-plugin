5674ffbcbf9f9bc35346d4d84395e37ac36457af
YT <g.tauchi@gmail.com>
2023-08-22 13:28:58 +0900

    nrf52　add

--- mcu_plugin.js
+++ mcu_plugin.js
@@ -1958,6 +1958,7 @@ module.exports = function(RED) {
             case "pico":
             case "gecko":
             case "qca4020":
+            case "nrf52":
             /* Not tested! */
 
             case "sim":
