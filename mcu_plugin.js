32f29281309df6e3495376c27b6b02520d32ed09
NW-Lab <g.tauchi@gmail.com>
2026-01-19 17:25:25 +0900

    add esp32/m5atom_s3r_m12

--- mcu_plugin.js
+++ mcu_plugin.js
@@ -644,6 +644,7 @@ module.exports = function(RED) {
             'esp32/m5atom_s3r',
             'esp32/m5atom_s3r_cam',
             'esp32/m5atom_s3r_echo_base',
+            'esp32/m5atom_s3r_m12',
             'esp32/m5atom_s3_org',
             'esp32/m5atom_u',
             'esp32/m5core_ink',
